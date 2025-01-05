ec4ea127dc3f4410cb5a6d6870201356b5c02c4d
Darrion Mills <dmills1994@gmail.com>
2025-01-04 20:53:26 -0400

    day 6 part 2

--- day06/main.go
+++ day06/main.go
@@ -91,6 +91,87 @@ func predictGuardPath(board Board, guard Guard) int {
 	return len(visited)
 }
 
+func predictGuardLoop(board Board, guard Guard) bool {
+	visited := map[string]int{}
+	done := false
+	looped := false
+	boardRows := len(board)
+	boardCols := len(board[0])
+
+	for !done {
+		visitedString := fmt.Sprintf("%d,%d", guard.position.x, guard.position.y)
+		count, exists := visited[visitedString]
+		if exists {
+			count++
+			visited[visitedString] = count
+
+			if count > 50 {
+				looped = true
+				done = true
+				continue
+			}
+		} else {
+			visited[visitedString] = 1
+		}
+
+		// If the guard reaches the edge of the board, exit
+		if (guard.position.x == 0 && guard.direction == "up") ||
+			(guard.position.x == boardRows-1 && guard.direction == "down") ||
+			(guard.position.y == 0 && guard.direction == "left") ||
+			(guard.position.y == boardCols-1 && guard.direction == "right") {
+			done = true
+			continue
+		}
+
+		nextPosition := guard.position
+		switch guard.direction {
+		case "up":
+			nextPosition.x -= 1
+			break
+
+		case "down":
+			nextPosition.x += 1
+			break
+
+		case "left":
+			nextPosition.y -= 1
+			break
+
+		case "right":
+			nextPosition.y += 1
+			break
+		}
+
+		if board[nextPosition.x][nextPosition.y] == "#" {
+			// If the next position is an obstacle
+			nextDirection := guard.direction
+			switch guard.direction {
+			case "up":
+				nextDirection = "right"
+				break
+
+			case "down":
+				nextDirection = "left"
+				break
+
+			case "left":
+				nextDirection = "up"
+				break
+
+			case "right":
+				nextDirection = "down"
+				break
+			}
+
+			guard.direction = nextDirection
+		} else {
+			guard.position = nextPosition
+		}
+	}
+
+	return looped
+}
+
 func main() {
 	inputFile := "input.txt"
 	file, err := os.Open(inputFile)
@@ -121,5 +202,29 @@ func main() {
 		}
 	}
 
-  fmt.Printf("Guard visited %d unique positions\n", predictGuardPath(board, guard))
+	fmt.Printf("Guard visited %d unique positions\n", predictGuardPath(board, guard))
+
+	loops := 0
+
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			currentPosition := board[i][j]
+			if currentPosition == "#" || currentPosition == "^" {
+				continue
+			}
+
+			newBoard := make([][]string, len(board))
+			for i := range newBoard {
+				newBoard[i] = make([]string, len(board[0]))
+				copy(newBoard[i], board[i])
+			}
+
+			newBoard[i][j] = "#"
+			if predictGuardLoop(newBoard, guard) {
+				loops++
+			}
+		}
+	}
+
+	fmt.Printf("Possible loops found: %d", loops)
 }
--- day06/main_test.go
+++ day06/main_test.go
@@ -20,9 +20,9 @@ func TestPredictGuardPath(t *testing.T) {
 			{"......#..."},
 		}
 
-    for index, str := range input {
-      input[index] = strings.Split(str[0], "")
-    }
+		for index, str := range input {
+			input[index] = strings.Split(str[0], "")
+		}
 
 		guard := Guard{position: Position{x: 6, y: 4}, direction: "up"}
 
@@ -34,4 +34,80 @@ func TestPredictGuardPath(t *testing.T) {
 		}
 	})
 
+}
+
+func TestPredictGuardLoop(t *testing.T) {
+	t.Run("example input", func(t *testing.T) {
+		input := [][]string{
+			{"....#....."},
+			{".........#"},
+			{".........."},
+			{"..#......."},
+			{".......#.."},
+			{".........."},
+			{".#..^....."},
+			{"........#."},
+			{"#........."},
+			{"......#..."},
+		}
+
+		for index, str := range input {
+			input[index] = strings.Split(str[0], "")
+		}
+
+		input[6][3] = "#"
+		guard := Guard{position: Position{x: 6, y: 4}, direction: "up"}
+
+		expected := true
+		actual := predictGuardLoop(input, guard)
+
+		if expected != actual {
+			t.Errorf("Error with predictGuardLoop")
+		}
+	})
+
+	t.Run("Test number of loops", func(t *testing.T) {
+		board := [][]string{
+			{"....#....."},
+			{".........#"},
+			{".........."},
+			{"..#......."},
+			{".......#.."},
+			{".........."},
+			{".#..^....."},
+			{"........#."},
+			{"#........."},
+			{"......#..."},
+		}
+		loops := 0
+		expected := 6
+
+		for index, str := range board {
+			board[index] = strings.Split(str[0], "")
+		}
+
+		for i := 0; i < len(board); i++ {
+			for j := 0; j < len(board[0]); j++ {
+				currentPosition := board[i][j]
+				if currentPosition == "#" || currentPosition == "^" {
+					continue
+				}
+
+				newBoard := make([][]string, len(board))
+				for i := range newBoard {
+					newBoard[i] = make([]string, len(board[0]))
+					copy(newBoard[i], board[i])
+				}
+				newBoard[i][j] = "#"
+				guard := Guard{position: Position{x: 6, y: 4}, direction: "up"}
+				if predictGuardLoop(newBoard, guard) {
+					loops++
+				} 
+			}
+		}
+
+		if expected != loops {
+			t.Errorf("Incorrect number of loops found, expected %d, got %d", expected, loops)
+		}
+	})
 }
