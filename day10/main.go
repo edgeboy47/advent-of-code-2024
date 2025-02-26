988e233a21c584b77cab721699bc378d1386c2c9
Darrion Mills <dmills1994@gmail.com>
2025-02-26 18:32:34 -0400

    day 10 part 2

--- day10/main.go
+++ day10/main.go
@@ -65,6 +65,56 @@ func trailHeadScore(maps [][]int, peaks *[]string, row, col int) int {
 	return score
 }
 
+func trailHeadRating(maps [][]int, row, col int) int {
+	maxRows := len(maps) - 1
+	maxCols := len(maps[0]) - 1
+
+	if col > maxCols || row > maxRows || col < 0 || row < 0 {
+		return 0
+	}
+
+	curr := maps[row][col]
+	if curr == 9 {
+		return 1
+	}
+
+	score := 0
+	if row > 0 {
+		up := maps[row-1][col]
+
+		if up == curr+1 {
+			score += trailHeadRating(maps, row-1, col)
+		}
+	}
+
+	if row < maxRows {
+		down := maps[row+1][col]
+
+		if down == curr+1 {
+			score += trailHeadRating(maps, row+1, col)
+		}
+	}
+
+	if col > 0 {
+		left := maps[row][col-1]
+
+		if left == curr+1 {
+			score += trailHeadRating(maps, row, col-1)
+		}
+
+	}
+
+	if col < maxCols {
+		right := maps[row][col+1]
+
+		if right == curr+1 {
+			score += trailHeadRating(maps, row, col+1)
+		}
+	}
+
+	return score
+}
+
 func main() {
 	inputFile := "input.txt"
 	file, err := os.Open(inputFile)
@@ -87,14 +137,17 @@ func main() {
 		topoMap = append(topoMap, newLine)
 	}
 	scores := 0
+	ratings := 0
 
 	for row := range topoMap {
 		for col := range topoMap[row] {
 			if topoMap[row][col] == 0 {
 				scores += trailHeadScore(topoMap, &[]string{}, row, col)
+				ratings += trailHeadRating(topoMap, row, col)
 			}
 		}
 	}
 
 	fmt.Printf("scores: %v", scores)
+	fmt.Printf("ratings: %d", ratings)
 }
