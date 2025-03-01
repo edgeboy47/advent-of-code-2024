644e9539b4fdc272d346c673f9405e2ef6f4d6b4
Darrion Mills <dmills1994@gmail.com>
2025-02-28 23:31:33 -0400

    day 11: reduce space complexity by using a map to store the list, add cache to reduce time complexity

--- day11/main.go
+++ day11/main.go
@@ -8,23 +8,57 @@ import (
 	"strings"
 )
 
-func blink(input []int) []int {
-	output := []int{}
+func addToMap(input map[int]int, key int) map[int]int {
+	if val, exists := input[key]; exists {
+		input[key] = val + 1
+	} else {
+		input[key] = 1
+	}
+	return input
+}
 
-	for _, val := range input {
-		if val == 0 {
-			output = append(output, 1)
-		} else if len(fmt.Sprintf("%d", val))%2 == 0 {
-			str := fmt.Sprintf("%d", val)
+// Store the list as a map, where the key is the number and the value is the count of that number in the list
+func blink(input map[int]int, cache map[int][]int) map[int]int {
+	output := make(map[int]int)
+	// fmt.Printf("Cache length: %d\n", len(cache))
+
+	for inputKey, inputValue := range input {
+		out, existsInCache := cache[inputKey]
+
+		// If the value exists in the cache, use it
+		if existsInCache {
+			for _, cacheVal := range out {
+				for range inputValue {
+					output = addToMap(output, cacheVal)
+				}
+			}
+			continue
+		}
+
+		if inputKey == 0 {
+			for range inputValue {
+				output = addToMap(output, 1)
+			}
+			cache[inputKey] = []int{1}
+		} else if len(fmt.Sprintf("%d", inputKey))%2 == 0 {
+			str := fmt.Sprintf("%d", inputKey)
 			left := str[0 : len(str)/2]
 			right := str[len(str)/2:]
 
 			leftVal, _ := strconv.Atoi(left)
 			rightVal, _ := strconv.Atoi(right)
 
-			output = append(output, leftVal, rightVal)
+			for range inputValue {
+				output = addToMap(output, leftVal)
+				output = addToMap(output, rightVal)
+			}
+			cache[inputKey] = []int{leftVal, rightVal}
 		} else {
-			output = append(output, val*2024)
+			calc := inputKey * 2024
+			for range inputValue {
+				output = addToMap(output, calc)
+			}
+			cache[inputKey] = []int{calc}
 		}
 	}
 
@@ -52,9 +86,30 @@ func main() {
 		}
 	}
 
-	for range 25 {
-		input = blink(input)
+	cache := make(map[int][]int)
+
+	final := 0
+
+	// Do each number from the input individually
+	for _, val := range input {
+		valueInput := map[int]int{
+			val: 1,
+		}
+
+		for i := range 75 {
+			valueLength := 0
+			valueInput = blink(valueInput, cache)
+
+			for _, val := range valueInput {
+				valueLength += val
+			}
+
+			fmt.Printf("Input %d length: %d\n", i, valueLength)
+		}
+		for _, val := range valueInput {
+			final += val
+		}
 	}
 
-	fmt.Printf("Length after 25 runs: %d", len(input))
+	fmt.Printf("Length after 25 runs: %d", final)
 }
--- day11/main_test.go
+++ day11/main_test.go
@@ -1,31 +1,45 @@
 package main
 
 import (
-	"slices"
+	"maps"
 	"testing"
 )
 
 func TestBlink(t *testing.T) {
 	t.Run("First Blink", func(t *testing.T) {
-		input := []int{125, 17}
-		expected := []int{253000, 1, 7}
-		actual := blink(input)
+		input := map[int]int{
+			125: 1,
+			17:  1,
+		}
+		expected := map[int]int{
+			253000: 1,
+			1:      1,
+			7:      1,
+		}
+		cache := make(map[int][]int)
+		actual := blink(input, cache)
 
-		if !slices.Equal(expected, actual) {
+		if !maps.Equal(expected, actual) {
 			t.Errorf("Expected %v, got %v", expected, actual)
 		}
 	})
 
 	t.Run("Length after 25 runs", func(t *testing.T) {
-		input := []int{125, 17}
+		input := map[int]int{
+			125: 1,
+			17:  1,
+		}
 		expected := 55312
+		cache := make(map[int][]int)
 		actual := 0
 
 		for range 25 {
-			input = blink(input)
+			input = blink(input, cache)
 		}
 
-		actual = len(input)
+		for _, val := range input {
+			actual += val
+		}
 
 		if expected != actual {
 			t.Errorf("Expected %d, got %d", expected, actual)
